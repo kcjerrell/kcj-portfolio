515126ad0447ef47f9d22e8169ed48b580a2e38b
Kelly Jerrell <kcjerrell@gmail.com>
2021-06-17 12:25:21 -0700

    changed files to tsx

---
+++ src/App.tsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import { useEffect, useState } from 'react';
+import './App.css';
+
+function App() {
+  const [state, setState] = useState({});
+  useEffect(() => {
+    async function getHello() {
+      const res = await fetch('/api/hello');
+      setState(JSON.stringify(res));
+    }
+    getHello();
+  }, []);
+
+  return (
+    <main>
+      <h1>I don't know</h1>
+      <p>{JSON.stringify(state)}</p>
+    </main>
+  );
+}
+
+export default App;
